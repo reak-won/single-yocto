a5ab42680bc6f3e86b2dea68fd244cdfc820a215
reak-won <lcykmk@gmail.com>
2022-08-31 21:11:25 +0900

    Core <-> Daemon UDP6 echo test OK
    [Desc] Core <-> Daemon tried to communicate each other(echo) and was successful. They are using IPv6.

--- source/sec/sec-core/src/sec-core.cpp
+++ source/sec/sec-core/src/sec-core.cpp
@@ -3,8 +3,48 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PORT 54321
+void die(int fd, char *s){
+	write(fd, s, strlen(s));
+	exit(1);
+}
 
 int main(){
-	int fd = open("/tmp/core-start", O_CREAT, 0777);
+	struct sockaddr_in6 server_addr, client_addr;
+	int sock, i;
+	socklen_t slen = sizeof(client_addr), recv_len;
+
+	char buf[128] = {0,};
+
+	int fd = open("/tmp/core-start", O_CREAT|O_RDWR, 0777);
+	write(fd,"hello\n",strlen("hello\n"));
+
+	if((sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+		die(fd, "socket");
+	write(fd, "socket ok\n",strlen("socket ok\n"));
+	
+	memset((char*)&server_addr, 0, sizeof(server_addr));
+	server_addr.sin6_family = AF_INET6;
+	server_addr.sin6_addr = in6addr_loopback;
+	server_addr.sin6_port = htons(PORT);
+
+	if(bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) die(fd, "bind");
+	write(fd, "bind ok\n", strlen("bind ok\n"));
+	while(1){
+		memset(buf, 0, sizeof(buf));
+		write(fd, "recv wait\n",strlen("recv wait\n"));
+		if((recv_len = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&client_addr, &slen)) == -1)
+			die(fd, "recvfrom()");
+
+		write(fd, buf, recv_len);
+		write(fd, "send to\n",strlen("send to\n"));
+		if(sendto(sock, buf, recv_len, 0, (struct sockaddr*) &client_addr, slen) == -1)
+			die(fd, "sendto()");
+	}
 	close(fd);
 }
--- source/sec/sec-daemon/src/main.cpp
+++ source/sec/sec-daemon/src/main.cpp
@@ -1,12 +1,58 @@
-#include <stdio.h>
+#include <cstdio>
 #include <unistd.h>
 #include <pthread.h>
 #include <fstream>
 #include <iostream>
+#include <thread>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <string.h>
 
+#define PORT 54321
+#define BUFLEN 128
 int main(){
-	std::ofstream ofs("/tmp/lock", std::ios::out |std::ios::app);
-	ofs<<"Hello!"<<std::endl;
+	std::ofstream ofs("/tmp/daemon_log", std::ios::out |std::ios::app);
+	ofs<<"daemon start"<<std::endl;
+	
+	struct sockaddr_in6 server_addr;
+	int sock, i;
+	socklen_t slen = sizeof(server_addr);
+	char buf[BUFLEN];
+	char message[BUFLEN];
+
+	int count = 0;
+	if( (sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1){
+		ofs<<"socket error"<<std::endl;
+		exit(1);
+	}
+	ofs<<"socket ok"<<std::endl;
+
+	memset((char*)&server_addr, 0, sizeof(server_addr));
+	server_addr.sin6_family = AF_INET6;
+	server_addr.sin6_port = htons(PORT);
+	server_addr.sin6_addr = in6addr_loopback;
+
+	while(1){
+		sleep(1);
+		count++;
+		sprintf(message, "count:%d\n",count);
+		ofs<<"message:"<<message<<std::endl;
+
+		if(sendto(sock, message, strlen(message), 0, (struct sockaddr *)&server_addr, slen) == -1){
+			ofs<<"sendto error"<<std::endl;
+			exit(1);
+		}
+		ofs<<"recv wait"<<std::endl;
+		memset(buf, '0', sizeof(buf));
+		if(recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&server_addr, &slen) == -1){
+			ofs<<"recvfrom error"<<std::endl;
+		}
+
+		ofs<<"buf:"<<buf<<std::endl;
+
+	}
+
 	ofs.close();
-	while(1);
 }
