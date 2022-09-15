55efdadadff0da245cb2369408fb23e14208ca3d
reak-won <lcykmk@gmail.com>
2022-09-15 19:06:01 +0900

    daemon and core udp code clean
    [Desc] daemon and core code is made clean.

--- source/sec/sec-core/src/sec-core.cpp
+++ source/sec/sec-core/src/sec-core.cpp
@@ -16,48 +16,103 @@
 
 #include <vector>
 #include <string>
+
 #define PORT 54321
+#define MAX_BUF_SIZE 4096
 
 int logfd;
-void die(char *s){
+char *log_msg;
+
+//UDP socket 
+struct sockaddr_in6 server_addr, client_addr;
+int sock;
+
+void die(const char *s){
 	write(logfd, s, strlen(s));
 	exit(1);
 }
 
+void log(const char *l){
+	write(logfd,"[CORE] ", 7);
+	write(logfd, l, strlen(l));
+}
+
+void log(uint8_t *l, size_t len){
+	write(logfd,"[CORE] ", 7);
+	write(logfd, l, len);
+}
 void udp_connect_test(){
 	struct sockaddr_in6 server_addr, client_addr;
 	int sock, i;
 	socklen_t slen = sizeof(client_addr), recv_len;
 
 	char buf[128] = {0,};
-
-	write(logfd,"hello\n",strlen("hello\n"));
-
 	if((sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-		die("socket");
-	write(logfd, "socket ok\n",strlen("socket ok\n"));
+		die("socket\n");
 	
 	memset((char*)&server_addr, 0, sizeof(server_addr));
 	server_addr.sin6_family = AF_INET6;
 	server_addr.sin6_addr = in6addr_loopback;
 	server_addr.sin6_port = htons(PORT);
 
-	if(bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) die("bind");
-	write(logfd, "bind ok\n", strlen("bind ok\n"));
+	if(bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) die("bind error\n");
+	
 	while(1){
 		memset(buf, 0, sizeof(buf));
-		write(logfd, "recv wait\n",strlen("recv wait\n"));
 		if((recv_len = recvfrom(sock, buf, sizeof(buf), 0, (struct sockaddr*)&client_addr, &slen)) == -1)
-			die("recvfrom()");
-
-		write(logfd, buf, recv_len);
-		write(logfd, "send to\n",strlen("send to\n"));
+			die("recvfrom()\n");
+		log("received\n");
+		log(buf);
+		
 		if(sendto(sock, buf, recv_len, 0, (struct sockaddr*) &client_addr, slen) == -1)
-			die("sendto()");
+			die("sendto()\n");
 	}
 
 }
 
+void create_udp_sock(){
+	int i;
+	socklen_t slen = sizeof(client_addr), recv_len; 
+	
+	if((sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+		die("socket error\n");
+	
+	log("socket ok\n");
+	
+	memset((char*)&server_addr, 0, sizeof(server_addr));
+
+	server_addr.sin6_family = AF_INET6;
+	server_addr.sin6_addr = in6addr_loopback;
+	server_addr.sin6_port = htons(PORT);
+
+}
+
+void bind_udp_sock(){
+	if(bind(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1)
+		die("bind error\n");
+	log("bind ok\n");
+}
+
+socklen_t recv_udp_msg(uint8_t *buf){
+
+	socklen_t slen, recv_len;
+	memset(buf, 0, MAX_BUF_SIZE);
+	log("recv wait\n");
+	if((recv_len = recvfrom(sock, buf, MAX_BUF_SIZE, 0, (struct sockaddr*)&client_addr, &slen)) == -1) die("recv from");
+
+	log(buf, recv_len);
+	
+	return recv_len;
+}
+
+ssize_t send_udp_msg(uint8_t *buf, size_t len){
+	socklen_t slen = sizeof(client_addr);
+	ssize_t sent_bytes;
+	if((sent_bytes = sendto(sock, buf, len, 0, (struct sockaddr*)&client_addr, slen)) == -1) 
+		die("sendto() error\n");
+	log("sendto\n");
+	return sent_bytes;	
+}
 void aes_test(){
 
 	uint8_t Key[32];
@@ -86,7 +141,8 @@ void aes_test(){
 	AES_cbc_encrypt(UserData, EncryptedData, UserDataSizePadded, (const AES_KEY*)AesKey,
 			IV, AES_ENCRYPT);
 	
-	write(logfd, "encrypted\n",strlen("encrypted\n"));
+	log("encrypted\n");
+	
 	for(int i=0;i<UserDataSizePadded;i++){
 		write(logfd, &EncryptedData[i], 1);
 	}
@@ -98,18 +154,32 @@ void aes_test(){
 	AES_cbc_encrypt(EncryptedData, DecryptedData, UserDataSizePadded, 
 			(const AES_KEY*)AesDecryptKey, IVd, AES_DECRYPT);
 
-	write(logfd, "\n",strlen("\n"));
-	write(logfd, "decrypted\n",strlen("decrypted\n"));
+	log("\ndecrypted");
+	
 	for(int i=0;i<UserDataSizePadded;i++)
 		write(logfd, &DecryptedData[i], 1);
 	
 }
 int main(){
 
+	int ret = 0;
+	uint8_t buf[MAX_BUF_SIZE] = {0,};
 	logfd = open("/tmp/core-start", O_CREAT|O_RDWR, 0777);
-	write(logfd,"hello\n",strlen("hello\n"));
-	//udp_connect_test(fd);
-	aes_test();
+
+	log("core started\n");
+
+	create_udp_sock();
+	bind_udp_sock();
+	
+	char logbuf[128]={0,};
+	while(1){
+		ssize_t len = recv_udp_msg(buf);
+		memset(logbuf, 0, 128);
+		sprintf(logbuf,"received len:%d\n",len);
+		log(logbuf);
+		buf[0]= 97;
+		send_udp_msg(buf,sizeof(buf));
+	}
 
 	close(logfd);
 }
--- source/sec/sec-daemon/src/main.cpp
+++ source/sec/sec-daemon/src/main.cpp
@@ -10,13 +10,77 @@
 #include <arpa/inet.h>
 #include <string.h>
 #include "../inc/test.h"
-
+#include <string>
 //CMakeLists.txt target_link_libraries & target_include_directories
 #include <sec_ipc.h>
 
+#define PORT 54321
+extern std::ofstream ofs;
+
+struct sockaddr_in6 server_addr;
+int sock;
+
+void die(const std::string &);
+void create_udp_sock();
+ssize_t send_udp_msg(const char*);
+socklen_t recv_udp_msg(char*, size_t);
+
+void log(const std::string &msg){
+	ofs<<"[DAEMON] "<<msg<<std::endl;
+}
+
 
 int main(){		
+	char msg[4096]={0, };
 	open_log_outputstream();
+	create_udp_sock();
+
+	int count = 0;
+	while(1){
+		sleep(1);
+		memset(msg, 0, sizeof(msg));
+		sprintf(msg, "count:%d\n",count++);
+
+		send_udp_msg(msg);
+		recv_udp_msg(msg, sizeof(msg));
+	}
 	//ipc_shm_test();
-	ipc_mq_test();
+	//ipc_mq_test();
+}
+
+
+void die(const std::string &m){
+	log(m);
+	exit(1);
+}
+void create_udp_sock(){
+	if((sock = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+		die("socket error");
+
+	memset((char*)&server_addr, 0, sizeof(server_addr));
+	server_addr.sin6_family = AF_INET6;
+	server_addr.sin6_port = htons(PORT);
+	server_addr.sin6_addr = in6addr_loopback;
+
+}
+
+ssize_t send_udp_msg(const char *msg){
+	socklen_t slen = sizeof(server_addr);
+	ssize_t sent_bytes;
+	if((sent_bytes = sendto(sock, msg, strlen(msg), 0, (struct sockaddr *)&server_addr, slen)) == -1){
+		die("sendto error\n");
+	}
+	return sent_bytes;
+
+}
+
+socklen_t recv_udp_msg(char *msg, size_t len){
+	
+	socklen_t slen,  recv_len;
+	if((recv_len = recvfrom(sock, msg, len, 0, (struct sockaddr*)&server_addr, &slen)) ==-1){
+		ofs<<"recvfrom error"<<std::endl;
+	}
+	log("received");
+	log(msg);
+	return recv_len;
 }
